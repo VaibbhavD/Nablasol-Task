e4190d109010a686697b090e7803e903da7a8db4
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-09-28 10:17:13 +0530

    wrap bt Context Provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { FormProvider } from "./Context/Task1.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+createRoot(document.getElementById("root")).render(
+  <FormProvider>
     <App />
-  </StrictMode>,
-)
+  </FormProvider>
+);
