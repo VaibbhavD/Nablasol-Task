9550a82cd6be489b3ccf4b3ae970492d6253e284
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-09-28 21:01:02 +0530

    make responsive and uopdate

--- src/components/StepNav.jsx
+++ src/components/StepNav.jsx
@@ -10,12 +10,12 @@ function StepNav() {
   const [currentStep, setCurrentStep] = useState(1); // Example, current step is 2
 
   return (
-    <div className="w-full max-w-5xl mx-auto">
-      <div className="flex justify-center mt-8 items-center bg-gray-200 rounded-t-lg">
+    <div className="w-full max-w-5xl mx-auto ">
+      <div className="flex justify-center mt-8 items-center bg-gray-200 ">
         {steps.map((step, index) => (
           <div
             key={index}
-            className={`py-3 sm:py-4 px-4 sm:px-6 transition-all flex-1 duration-300 flex items-center justify-center text-center ${
+            className={`py-3 sm:py-4 px-4 sm:px-6 transition-all flex-1 duration-300 flex items-center justify-center text-center text-xs lg:text-lg ${
               currentStep >= step.id
                 ? "bg-blue-400 text-white"
                 : "bg-gray-200 text-gray-500"
@@ -23,10 +23,10 @@ function StepNav() {
               step.id === currentStep && currentStep != 3
                 ? "rounded-r-full"
                 : ""
-            }`}
+            } ${step.id === 1 ? "rounded-tl-3xl" : ""}`}
           >
             <span
-              className={`p-1 sm:p-2 px-2 sm:px-2.5 rounded-full text-sm sm:text-lg font-bold flex items-center justify-center ${
+              className={`p-1 sm:p-2 px-2 sm:px-2.5 rounded-full  font-bold flex items-center justify-center ${
                 currentStep >= step.id
                   ? "bg-white text-black"
                   : "bg-gray-300 text-white"
@@ -34,9 +34,7 @@ function StepNav() {
             >
               {step.id}
             </span>
-            <span className="ml-2 sm:ml-4 text-xs sm:text-base">
-              {step.label}
-            </span>
+            <span className="ml-2 sm:ml-4 ">{step.label}</span>
           </div>
         ))}
       </div>
