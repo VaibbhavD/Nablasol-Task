a232d44da5d2e0a9e0a5210f1e970c556d2de37b
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-09-28 10:16:28 +0530

    Create Task 1 Context and define states.

---
+++ src/Context/Task1.jsx
@@ -0,0 +1,31 @@
+import React, { createContext, useContext, useState } from "react";
+
+const FormContext = createContext();
+
+export const useFormContext = () => {
+  return useContext(FormContext);
+};
+
+export const FormProvider = ({ children }) => {
+  const [formData, setFormData] = useState({
+    projectName: "",
+    client: "",
+    newClients: [],
+    startDate: "",
+    endDate: "",
+    notes: "",
+  });
+
+  const updateFormData = (field, value) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      [field]: value,
+    }));
+  };
+
+  return (
+    <FormContext.Provider value={{ formData, updateFormData }}>
+      {children}
+    </FormContext.Provider>
+  );
+};
