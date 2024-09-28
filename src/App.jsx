3402f438f7d08ef1d882572bd7d42b125c0fb42e
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-09-28 13:31:46 +0530

    add routes nd also Aos uning useeffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
-import { useState } from "react";
-import "./App.css";
+import { useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import Dashboard from "./Dashboard/index";
 import Task1 from "./Task1/index";
+import Summary from "./Task1/components/Summury";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import "./App.css";
 
 function App() {
-  return <Task1 />;
+  useEffect(() => {
+    Aos.init({ duration: 800, easing: "ease-in-out" });
+  }, []);
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+
+        <Route path="/task1" element={<Task1 />} />
+
+        <Route path="/summary" element={<Summary />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
