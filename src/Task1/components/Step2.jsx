97b1fa82f2804952baf90ab182d1c63e8af54d7d
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-09-28 10:31:09 +0530

    Make dynamic and also form sata update states and also handlig error  whilw input field noy have

--- src/Task1/components/Step2.jsx
+++ src/Task1/components/Step2.jsx
@@ -1,117 +1,185 @@
 import React, { useState } from "react";
 import Navigation from "../../components/Navigation";
+import { useFormContext } from "../../Context/Task1";
 
 const Step2 = ({ onNext, onBack, Step }) => {
-  const [selectedTab, setSelectedTab] = useState("time_and_materials");
-  const [hourlyRate, setHourlyRate] = useState("12678.00");
-  const [budgetAlert, setBudgetAlert] = useState("80");
-  const [budgetResets, setBudgetResets] = useState(false);
-  const [sendEmailAlert, setSendEmailAlert] = useState(false);
+  const { formData, updateFormData } = useFormContext();
+  const [errors, setErrors] = useState({});
+
+  // Validation function
+  const validateFields = () => {
+    const newErrors = {};
+
+    // Validate Project Hourly Rate Dropdown
+    if (!formData.projectHourlyRate)
+      newErrors.projectHourlyRate = "Please select a project hourly rate.";
+
+    // Validate Hourly Rate Input
+    if (!formData.hourlyRate) newErrors.hourlyRate = "Hourly rate is required.";
+    else if (isNaN(parseFloat(formData.hourlyRate)))
+      newErrors.hourlyRate = "Please enter a valid number.";
+
+    // Validate Budget Dropdown
+    if (!formData.budgetType)
+      newErrors.budgetType = "Please select a budget type.";
+
+    // Validate Budget Alert Input (if checkbox is checked)
+    if (formData.sendEmailAlert) {
+      if (!formData.budgetAlert)
+        newErrors.budgetAlert = "Budget alert is required.";
+      else if (isNaN(parseInt(formData.budgetAlert)))
+        newErrors.budgetAlert = "Please enter a valid percentage.";
+    }
+
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+
+  const handleNext = () => {
+    if (validateFields()) {
+      onNext();
+    }
+  };
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100 p-4">
-      <div className="bg-white shadow-lg rounded-lg p-8 w-full max-w-md ">
+      <div className="bg-white shadow-lg rounded-lg p-8 w-full max-w-md">
         <h2 className="text-xl font-bold text-center mb-4">Project Type</h2>
         <p className="text-center text-sm text-gray-600 mb-6">
           Don’t panic — You can also customize this type in settings.
         </p>
 
         {/* Tabs for Project Types */}
-        <div className="flex flex-col sm:flex-row justify-around mb-6 ">
+        <div className="flex flex-col sm:flex-row justify-around mb-6">
           {["time_and_materials", "fixed_fee", "non_billable"].map((tab) => (
             <button
               key={tab}
               className={`w-full sm:w-1/3 py-2 rounded text-sm mb-2 sm:mb-0 p-1 ${
-                selectedTab === tab
+                formData.selectedTab === tab
                   ? "bg-blue-500 text-white"
                   : "bg-gray-100 text-gray-600"
               }`}
-              onClick={() => setSelectedTab(tab)}
+              onClick={() => updateFormData("selectedTab", tab)}
             >
               {tab.replace(/_/g, " ").replace(/^\w/, (c) => c.toUpperCase())}
             </button>
           ))}
         </div>
 
-        {/* Form Section */}
-        <div>
-          {/* Hourly Rate Section */}
-          <div className="mb-4">
-            <label className="block text-gray-700 font-medium">
-              Hourly Rate
-            </label>
-            <p className="text-sm text-gray-500 mb-2">
-              We need hourly rates to track your project's billable amount.
-            </p>
-            <div className="flex flex-col sm:flex-row items-center w-full">
-              <select className="border border-gray-300 rounded-l p-2 mb-2 sm:mb-0 sm:mr-2 w-full md:w-1/2 focus:outline-none focus:ring focus:ring-blue-200">
-                <option>Project Hourly Rate</option>
-              </select>
-              <input
-                type="text"
-                value={hourlyRate}
-                onChange={(e) => setHourlyRate(e.target.value)}
-                className="border border-gray-300 rounded p-2 w-full md:w-1/4 focus:outline-none focus:ring focus:ring-blue-200"
-              />
-            </div>
-          </div>
-
-          {/* Budget Section */}
-          <div className="mb-4">
-            <label className="block text-gray-700 font-medium">Budget</label>
-            <p className="text-sm text-gray-500 mb-2">
-              We need hourly rates to track your project's billable amount.
-            </p>
-            <select className="border border-gray-300 rounded p-2 md:w-1/2 focus:outline-none focus:ring focus:ring-blue-200">
-              <option>Hours per Person</option>
-              <option>Total Hours</option>
-              <option>Fixed Budget</option>
+        {/* Hourly Rate Section */}
+        <div className="mb-4">
+          <label className="block text-gray-700 font-medium">Hourly Rate</label>
+          <p className="text-sm text-gray-500 mb-2">
+            We need hourly rates to track your project's billable amount.
+          </p>
+          <div className="flex flex-col sm:flex-row items-center w-full">
+            <select
+              className={`border border-gray-300 rounded-l p-2 mb-2 sm:mb-0 sm:mr-2 w-full md:w-1/2 focus:outline-none focus:ring focus:ring-blue-200 ${
+                errors.projectHourlyRate ? "border-red-500" : "border-gray-300"
+              }`}
+              value={formData.projectHourlyRate}
+              onChange={(e) =>
+                updateFormData("projectHourlyRate", e.target.value)
+              }
+            >
+              <option value="">Select Project Hourly Rate</option>
+              <option value="project_hourly_rate_1">Rate 1</option>
+              <option value="project_hourly_rate_2">Rate 2</option>
             </select>
-          </div>
 
-          {/* Checkbox for Budget Resets */}
-          <div className="mb-4">
-            <label className="inline-flex items-center">
+            <input
+              type="text"
+              value={formData.hourlyRate}
+              onChange={(e) => updateFormData("hourlyRate", e.target.value)}
+              className={`border p-2 w-full md:w-1/4 rounded ${
+                errors.hourlyRate ? "border-red-500" : "border-gray-300"
+              } focus:outline-none focus:ring focus:ring-blue-200`}
+            />
+            {errors.hourlyRate && (
+              <p className="text-red-500 text-sm mt-1">{errors.hourlyRate}</p>
+            )}
+          </div>
+          {errors.projectHourlyRate && (
+            <p className="text-red-500 text-sm mt-1">
+              {errors.projectHourlyRate}
+            </p>
+          )}
+        </div>
+
+        {/* Budget Section */}
+        <div className="mb-4">
+          <label className="block text-gray-700 font-medium">Budget</label>
+          <p className="text-sm text-gray-500 mb-2">
+            We need hourly rates to track your project's billable amount.
+          </p>
+          <select
+            className={`border border-gray-300 rounded p-2 md:w-1/2 focus:outline-none focus:ring focus:ring-blue-200 ${
+              errors.budgetType ? "border-red-500" : "border-gray-300"
+            }`}
+            value={formData.budgetType}
+            onChange={(e) => updateFormData("budgetType", e.target.value)}
+          >
+            <option value="">Select Budget Type</option>
+            <option value="hours_per_person">Hours per Person</option>
+            <option value="total_hours">Total Hours</option>
+            <option value="fixed_budget">Fixed Budget</option>
+          </select>
+          {errors.budgetType && (
+            <p className="text-red-500 text-sm mt-1">{errors.budgetType}</p>
+          )}
+        </div>
+
+        {/* Checkbox for Budget Resets */}
+        <div className="mb-4">
+          <label className="inline-flex items-center">
+            <input
+              type="checkbox"
+              checked={formData.budgetResets}
+              onChange={(e) => updateFormData("budgetResets", e.target.checked)}
+              className="h-4 w-4 text-blue-600 border-gray-300 rounded focus:ring focus:ring-blue-200"
+            />
+            <span className="ml-2 text-sm text-gray-700">
+              Budget resets every month
+            </span>
+          </label>
+        </div>
+
+        {/* Budget Alert Section */}
+        <div className="mb-6">
+          <div className="flex flex-col sm:flex-row items-center justify-between">
+            {/* Checkbox and label */}
+            <label className="inline-flex items-center text-sm text-gray-700 mb-2 sm:mb-0">
               <input
                 type="checkbox"
-                checked={budgetResets}
-                onChange={(e) => setBudgetResets(e.target.checked)}
-                className="h-4 w-4 text-blue-600 border-gray-300 rounded focus:ring focus:ring-blue-200"
+                className="h-4 w-4 text-blue-600 border-gray-300 rounded focus:ring focus:ring-blue-200 mr-2"
+                checked={formData.sendEmailAlert}
+                onChange={(e) =>
+                  updateFormData("sendEmailAlert", e.target.checked)
+                }
               />
-              <span className="ml-2 text-sm text-gray-700">
-                Budget resets every month
-              </span>
-            </label>
-          </div>
-
-          {/* Budget Alert Section */}
-          <div className="mb-6">
-            <div className="flex flex-col sm:flex-row items-center justify-between">
-              {/* Checkbox and label */}
-              <label className="inline-flex items-center text-sm text-gray-700 mb-2 sm:mb-0">
+              <div>
+                Send email alerts if project exceeds
                 <input
-                  type="checkbox"
-                  className="h-4 w-4 text-blue-600 border-gray-300 rounded focus:ring focus:ring-blue-200 mr-2"
-                  checked={sendEmailAlert}
-                  onChange={(e) => setSendEmailAlert(e.target.checked)}
+                  type="text"
+                  value={formData.budgetAlert}
+                  onChange={(e) =>
+                    updateFormData("budgetAlert", e.target.value)
+                  }
+                  className={`w-16 text-center border p-1 mx-2 rounded ${
+                    errors.budgetAlert ? "border-red-500" : "border-gray-300"
+                  } focus:outline-none focus:ring focus:ring-blue-200`}
                 />
-                <div>
-                  Send email alerts if project exceeds
-                  <input
-                    type="text"
-                    value={budgetAlert}
-                    onChange={(e) => setBudgetAlert(e.target.value)}
-                    className="w-16 text-center border border-gray-300 rounded p-1 mx-2 focus:outline-none focus:ring focus:ring-blue-200"
-                  />
-                  <span className="text-sm ml-1"> % of budget</span>
-                </div>
-              </label>
-            </div>
+                <span className="text-sm ml-1"> % of budget</span>
+              </div>
+            </label>
+            {errors.budgetAlert && (
+              <p className="text-red-500 text-sm mt-1">{errors.budgetAlert}</p>
+            )}
           </div>
-
-          {/* Navigation Buttons */}
-          <Navigation onNext={onNext} onBack={onBack} Step={Step} />
         </div>
+
+        {/* Navigation Buttons */}
+        <Navigation onNext={handleNext} onBack={onBack} Step={Step} />
       </div>
     </div>
   );
